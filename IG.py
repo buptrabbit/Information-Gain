59c33d86bb23782e2e1bdbe74226b7e503432498
moonlight <buptgogo@163.com>
2013-12-20 16:21:30 +0800

    Update IG.py

--- IG.py
+++ IG.py
@@ -1,38 +1,65 @@
 import math
 
-def get_pc(inputpath):
-    cnt_c = [0]*33
-    dic = {}
-    clsList = ['']*33
-    for line in open(inputpath):
-        line,cls = line.rstrip('\n').rsplit('\t',1)
-        cnt_c[int(cls)-1] += 1
-        clsList[int(cls)-1] += (line+'\t')
-    Dtotal = float(sum(cnt_c))
-    for i in range(33):
-        dic[i+1] = [cnt_c[i],cnt_c[i]/Dtotal]
-    pc = [v for [k,v] in dic.values()]
-    Hc = -sum([(lambda p: p*math.log(p,2))(p) for p in pc])
-    return Hc,Dtotal,clsList,cnt_c
+def info_gain(x,y,k=None):
+    num_d = len(y)
+    num_ck = {}
+    num_fi_ck = {}
+    num_nfi_ck = {}
+    for xi,yi in zip(x,y):
+        num_ck[yi] = num_ck.get(yi,0)+1
+        for index,xii in enumerate(xi):
+            if not num_fi_ck.has_key(index):
+                num_fi_ck[index] = {}
+                num_nfi_ck[index] = {}    
+            if not num_fi_ck[index].has_key(yi):
+                num_fi_ck[index][yi] = 0
+                num_nfi_ck[index][yi] = 0
+            if not xii==0:
+                num_fi_ck[index][yi] = num_fi_ck[index].get(yi)+1
+            else:
+                num_nfi_ck[index][yi] = num_nfi_ck[index].get(yi)+1
+    num_fi = {}
+    for fi,dic in num_fi_ck.items():
+        num_fi[fi] = sum(dic.values())
+    num_nfi = dict([(fi,num_d-num) for fi,num in num_fi.items()])
+    HD = 0
+    for ck,num in num_ck.items():
+        p = float(num)/num_d
+        HD = HD-p*math.log(p,2)
+    IG = {}
+    for fi in num_fi_ck.keys():
+        POS = 0
+        for yi,num in num_fi_ck[fi].items():
+            p = (float(num)+0.0001)/(num_fi[fi]+0.0001*len(dic))
+            POS = POS - p*math.log(p,2)
+        
+        NEG = 0
+        for yi,num in num_nfi_ck[fi].items():
+            p = (float(num)+0.0001)/(num_nfi[fi]+0.0001*len(dic))
+            NEG = NEG - p*math.log(p,2)
+        p = float(num_fi[fi])/num_d
+        IG[fi] = round(HD - p*POS - (1-p)*NEG,4)
+    IG = sorted(IG.items(),key=lambda d:d[1],reverse=True)
+    if k==None:
+        return IG
+    else:
+        return IG[0:k]
 
-def gen_ig(Hc,Dtotal,clsList,cnt_c):
-    dic = {}
-    dic_ig = {}
-    for i in range(33):
-        lst = clsList[i].split('\t')
-        for l in lst:
-            d = jieba.analyse.extract_tags(l,2)
-            for k in d.keys():
-                if fltr(k):
-                    continue
-                if dic.has_key(k):
-                    dic[k][i] += 1
-                else:
-                    dic[k] = [0]*33
-                    dic[k][i] += 1
-    for k in dic.keys():
-        pt = sum(dic[k])/Dtotal
-        pc_t = [nc/float(sum(dic[k])) for nc in dic[k]]
-        pc_t_ = [(nc-nct)/(Dtotal-float(sum(dic[k]))) for (nc,nct) in zip(cnt_c,dic[k])]
-        dic_ig[k] = Hc+pt*sum([(lambda p: p*math.log(p,2))(p) for p in pc_t if not p==0])+(1-pt)*sum([(lambda p: p*math.log(p,2))(p) for p in pc_t_ if not p==0])
-    return dic_ig
+def red(x,IG):
+    feature = dict.fromkeys([fi for fi,v in IG])
+    newx = []
+    for xi in x:
+        newrow = []
+        for index,xii in enumerate(xi):
+            if feature.has_key(index):
+                newrow.append(xii)
+        newx.append(newrow)
+    return newx
+#test
+'''
+x = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1],[0,1,0,0]]
+y = [1,2,3,2,2]
+IG = info_gain(x,y,2)
+for k,v in IG:
+    print k,v
+'''
